e87248053b113136267f45523d70056ab4bbff52
User <asqarservis0000@gmail.com>
2025-07-06 19:54:34 +0300

    update

--- main.py
+++ main.py
@@ -1,43 +1,52 @@
 import asyncio
 import logging
 import sys
+import bcrypt
 
-from aiogram.types import BotCommand
-
-from bot.handlera.main_handler import CustomMiddleware
-from bot.handlera import *
 from aiogram import Bot
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
+from aiogram.types import BotCommand
 from aiogram.utils.i18n import I18n, FSMI18nMiddleware
+
+from bot.handlera.main_handler import CustomMiddleware
+from bot.handlera import dp
 from utils.env_data import BotConfig
+from db.model import db
 
 TOKEN = BotConfig.TOKEN
 
+async def on_startup(bot: Bot):
+    # DB yaratish (birinchi run ichida)
+    await db.init()
+    await db.create_all()
 
-from db.model import db
-db.init()
-asyncio.run(db.create_all())
-import bcrypt
-print(bcrypt.hashpw("3".encode(), salt=bcrypt.gensalt()))
-
-async def set_bot_commands(bot: Bot):
+    # Bot komandalarini qo‘shish
     commands = [
-        BotCommand(command="/start", description="Starting bot."),
-        BotCommand(command="/channel", description="Send channels id")
+        BotCommand(command="/start", description="Start the bot"),
+        BotCommand(command="/channel", description="Send channel ID"),
     ]
-    await bot.set_my_commands(commands=commands)
+    await bot.set_my_commands(commands)
 
+    # Hash parol misol (DEBUG)
+    print(bcrypt.hashpw("3".encode(), salt=bcrypt.gensalt()))
 
-async def main() -> None:
+async def main():
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+
+    # Bot va Dispatcher
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    # Til middleware
     i18n = I18n(path='locales', default_locale='uz', domain='messages')
     dp.update.outer_middleware(FSMI18nMiddleware(i18n))
     dp.message.outer_middleware(CustomMiddleware())
-    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-    await set_bot_commands(bot)
+
+    # On startup
+    await on_startup(bot)
+
+    # Polling
     await dp.start_polling(bot)
 
-
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
